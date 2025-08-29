75c91e4386854a2e29a7c5a9f308670b3cb29368
Ezequiel Teotônio Jó <41303804+ezequieltjo@users.noreply.github.com>
2025-08-29 01:47:47 +0000

    adicionado barra lateral com menu do site e 3 paginas

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,23 +1,63 @@
 import streamlit as st
 from PIL import Image
 
-st.set_page_config(
-    page_title="Otimização CODE",
-    page_icon="🧑🏽‍💻",
-    layout="wide" 
-)
+if 'pagina_atual' not in st.session_state:
+    st.session_state.pagina_atual = 'home'
 
-col1, col2, col3 = st.columns([1, 3, 1])
+# --- BARRA LATERAL COM BOTÕES ---
+with st.sidebar:
+    st.header("Menu")
+    
+    # Atualiza o estado da sessão com base no botão clicado
+    if st.button("Início", use_container_width=True):
+        st.session_state.pagina_atual = 'home'
 
-with col2:
-    # Adicionando imagem do projeto CODE
-    try:
-        imgProjeto = Image.open("code-programacao.png")
-        st.image(imgProjeto, use_container_width=True)
-    except FileNotFoundError:
-        st.error("Imagem do projeto não encontrada!")
+    if st.button("Configurações", use_container_width=True):
+        st.session_state.pagina_atual = 'config'
 
-    st.markdown("<h1 style='text-align: center; color: #da21ff;'>Otimização CODE</h1>", unsafe_allow_html=True)
-    st.markdown("<p style='text-align: center;'>Uma ferramenta que otimiza a alocação dos tutores às escolas do projeto.</p>", unsafe_allow_html=True)
-    st.markdown("<p style='text-align: center;'>Projeto de Trabalho de Conclusão de Curso do discente Ezequiel Teotônio Jó.</p>", unsafe_allow_html=True)
+    if st.button("Informações", use_container_width=True):
+        st.session_state.pagina_atual = 'info'
+        
+    st.info("Projeto de Trabalho de Conclusão de Curso do discente Ezequiel Teotônio Jó.")
 
+# --- DECLARAÇÃO DAS PÁGINAS ---
+if st.session_state.pagina_atual == "home":
+
+    st.set_page_config(
+        page_title="Otimização CODE",
+        page_icon="🧑🏽‍💻",
+        layout="wide" 
+    )
+
+    col1, col2, col3 = st.columns([1, 3, 1])
+
+    with col2:
+        # Adicionando imagem do projeto CODE
+        try:
+            imgProjeto = Image.open("code-programacao.png")
+            st.image(imgProjeto, use_container_width=True)
+        except FileNotFoundError:
+            st.error("Imagem do projeto não encontrada!")
+
+        st.markdown("<h1 style='text-align: center; color: #eb8334;'>Otimização CODE</h1>", unsafe_allow_html=True)
+        st.markdown("<p style='text-align: center;'>Uma ferramenta que otimiza a alocação dos tutores às escolas do projeto.</p>", unsafe_allow_html=True)
+        st.markdown("<p style='text-align: center;'>Projeto de Trabalho de Conclusão de Curso do discente Ezequiel Teotônio Jó.</p>", unsafe_allow_html=True)
+
+if st.session_state.pagina_atual == "config":
+    st.set_page_config(
+        page_title="Configurações - Otimização CODE ",
+        page_icon="🧑🏽‍💻",
+        layout="wide" 
+    )
+
+    col1, col2, col3 = st.columns([1, 3, 1])
+
+
+if st.session_state.pagina_atual == "info":
+    st.set_page_config(
+        page_title="Informações - Otimização CODE ",
+        page_icon="🧑🏽‍💻",
+        layout="wide" 
+    )
+
+    col1, col2, col3 = st.columns([1, 3, 1])
