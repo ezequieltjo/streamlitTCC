37528908ddb8c3593f99f5b7ac728a86a624bcbd
Ezequiel Teotônio Jó <41303804+ezequieltjo@users.noreply.github.com>
2025-09-01 01:16:50 +0000

    melhorado organização das telas, adicionado upload de arquivos

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,8 +11,8 @@ st.set_page_config(
     layout="wide" 
     )
 
-if 'pagina_atual' not in st.session_state:
-    st.session_state.pagina_atual = 'home'
+if 'current_page' not in st.session_state:
+    st.session_state.current_page = 'home'
 
 # --- BARRA LATERAL COM BOTÕES ---
 with st.sidebar:
@@ -20,20 +20,21 @@ with st.sidebar:
     
     # Atualiza o estado da sessão com base no botão clicado
     if st.button("Início", width="stretch"):
-        st.session_state.pagina_atual = 'home'
+        st.session_state.current_page = 'home'
 
     if st.button("Configurações", width="stretch"):
-        st.session_state.pagina_atual = 'config'
+        st.session_state.current_page = 'config'
 
     if st.button("Informações", width="stretch"):
-        st.session_state.pagina_atual = 'info'
+        st.session_state.current_page = 'info'
         
     st.info("Projeto de Trabalho de Conclusão de Curso do discente Ezequiel Teotônio Jó.")
 
 # --- DECLARAÇÃO DAS PÁGINAS ---
-if st.session_state.pagina_atual == "home":
+# ------------------ HOME ------------------
+if st.session_state.current_page == "home":
 
-    col1, col2, col3 = st.columns([1, 3, 1])
+    col1, col2, col3 = st.columns([1, 10, 1])
 
     with col2:
         # Adicionando imagem do projeto CODE
@@ -47,70 +48,99 @@ if st.session_state.pagina_atual == "home":
         st.markdown("<p style='text-align: center;'>Uma ferramenta que otimiza a alocação dos tutores às escolas do projeto.</p>", unsafe_allow_html=True)
         #st.markdown("<p style='text-align: center;'>Projeto de Trabalho de Conclusão de Curso do discente Ezequiel Teotônio Jó.</p>", unsafe_allow_html=True)
 
-        # Simulação de um processo de otimização   
-        if st.button("Otimizar", width="content"):
+        # A coluna do meio (btn_col2) será onde colocaremos o botão.
+        btn_col1, btn_col2, btn_col3 = st.columns([2, 1, 2]) 
 
-            with st.spinner("Otimizando... Por favor, aguarde."):
-                time.sleep(3)
-                st.success("Otimização concluída!", icon="✅")
-                    
-            # Exibir resultados simulados no arquivo 'resultados.csv'
-            st.subheader("Resultados da Otimização")
-            try:
-                alocacao = pd.read_csv("alocacoes.csv")
-                st.dataframe(alocacao)
-            except FileNotFoundError:
-                st.error("Arquivo 'alocacoes.csv' não encontrado!")
+        with btn_col2:
+            # Simulação de um processo de otimização   
+            if st.button("Otimizar", width="content"):
+                with st.spinner("Otimizando... Por favor, aguarde."):
+                    time.sleep(3)
+                    #st.success("Otimização concluída!", icon="✅")
+                        
+        # Exibir resultados simulados no arquivo 'resultados.csv'
+        st.subheader("Resultados da Otimização")
+        try:
+            alocacao = pd.read_csv("alocacoes.csv")
+            st.dataframe(alocacao)
+        except FileNotFoundError:
+            st.error("Arquivo 'alocacoes.csv' não encontrado!")
 
-if st.session_state.pagina_atual == "config":
-
-    col1, col2, col3 = st.columns([1, 3, 1])
+# ------------------ CONFIGURAÇÕES ------------------
+if st.session_state.current_page == "config":
 
     st.markdown("<h1 style='text-align: center; color: #eb8334;'>Configurações</h1>", unsafe_allow_html=True)
     st.markdown("<p style='text-align: center;'>Configure os parâmetros do algoritmo.</p>", unsafe_allow_html=True)
     st.markdown("<p style='text-align: center;'>Para mais informações consulte a página de Informações.</p>", unsafe_allow_html=True)
     
-    pref1 = st.number_input("Pontuação da 1º Preferência de Escola:", min_value=0, value= 8000, icon="🥇")
-    #st.write("Primeira Preferência:", pref1)
+    col1, col2, col3, col4 = st.columns([1, 5, 5, 1])
 
-    pref2 = st.number_input("Pontuação da 2º Preferência Escola:", min_value=0, value= 7000, icon="🥈")
-    #st.write("Primeira Preferência:", pref2)
+    with col2:
+        st.markdown("##### Arquivos de Entrada")
+        st.markdown("Faça o upload dos arquivos CSV contendo os dados dos tutores e das escolas:")
+        tutors_file = st.file_uploader("Upload do arquivo de Tutores (CSV)", type=["csv"], key="tutors_uploader")
 
-    pref3 = st.number_input("Pontuação da 3º Preferência Escola:", min_value=0, value= 6000, icon="🥉")
-    #st.write("Primeira Preferência:", pref3)
+        if tutors_file is not None:
+            try:
+                tutors_df = pd.read_csv(tutors_file)
+                st.success("Arquivo de Tutores carregado com sucesso!", icon="✅")
+                #st.dataframe(tutors_df.head())
+            except Exception as e:
+                st.error(f"Erro ao ler o arquivo de Tutores: {e}")
 
-    baseDistance = st.number_input("Pontuação Base da Preferência por Distâncias:", min_value=0, value= 5000, icon="🗺️")
+        schools_file = st.file_uploader("Upload do arquivo de Escolas (CSV)", type=["csv"], key="schools_uploader")
+        if schools_file is not None:
+            try:
+                schools_df = pd.read_csv(schools_file)
+                st.success("Arquivo de Escolas carregado com sucesso!", icon="✅")
+                #st.dataframe(schools_df.head())
+            except Exception as e:
+                st.error(f"Erro ao ler o arquivo de Escolas: {e}")
 
-    baseRanking = st.number_input("Pontuação Base para o Ranking dos Tutores:", min_value=0, value= 1000000000, icon="🏆")
+    with col3: 
+        st.markdown("##### Parâmetros do Algoritmo de Otimização")
+        st.markdown("Ajuste os parâmetros que influenciam a alocação dos tutores às escolas:")
 
-    decayType = 'sigmoid'
+        pref1 = st.number_input("Pontuação da 1º Preferência de Escola:", min_value=0, value= 8000, icon="🥇")
+        #st.write("Primeira Preferência:", pref1)
 
-    st.markdown("###### Tipo de Decaimento da Pontuação por Distância:")
-    # Cria um seletor de rádio na horizontal
-    decayOptions = ['Sigmoide', 'Linear']
-    decayType = st.radio(
-        "Escolha o tipo de decaimento:", 
-        options=decayOptions, 
-        horizontal=True, 
-        label_visibility="collapsed",
-        width="stretch"
-    )
+        pref2 = st.number_input("Pontuação da 2º Preferência Escola:", min_value=0, value= 7000, icon="🥈")
+        #st.write("Primeira Preferência:", pref2)
 
-    if decayType == 'Sigmoide':
+        pref3 = st.number_input("Pontuação da 3º Preferência Escola:", min_value=0, value= 6000, icon="🥉")
+        #st.write("Primeira Preferência:", pref3)
+
+        baseDistance = st.number_input("Pontuação Base da Preferência por Distâncias:", min_value=0, value= 5000, icon="🗺️")
+
+        baseRanking = st.number_input("Pontuação Base para o Ranking dos Tutores:", min_value=0, value= 1000000000, icon="🏆")
+
         decayType = 'sigmoid'
-    else:
-        decayType = 'linear'
 
-    sigmoidCurve = st.number_input("Escala de Inclinação da Curva Sigmoide:", min_value=0, value=2000, icon="📉")
+        st.markdown("###### Tipo de Decaimento da Pontuação por Distância:")
+        # Cria um seletor de rádio na horizontal
+        decayOptions = ['Sigmoide', 'Linear']
+        decayType = st.radio(
+            "Escolha o tipo de decaimento:", 
+            options=decayOptions, 
+            horizontal=True, 
+            label_visibility="collapsed",
+            width="stretch"
+        )
+
+        if decayType == 'Sigmoide':
+            decayType = 'sigmoid'
+        else:
+            decayType = 'linear'
+
+        sigmoidCurve = st.number_input("Escala de Inclinação da Curva Sigmoide:", min_value=0, value=2000, icon="📉")
 
     #st.write("Configurações: ", pref1, "/", pref2, "/", pref3, "/", baseDistance, "/", baseRanking, "/", decayType, "/", sigmoidCurve)
 
-
-if st.session_state.pagina_atual == "info":
-
-    col1, col2, col3 = st.columns([1, 3, 1])
-
+# ------------------ INFORMAÇÕES ------------------
+if st.session_state.current_page == "info":
     st.markdown("<h1 style='text-align: center; color: #eb8334;'>Informações</h1>", unsafe_allow_html=True)
+
+    col1, col2, col3 = st.columns([1, 10, 1])
 
     with col2:
         st.markdown("### Sobre o Projeto")
@@ -127,7 +157,7 @@ if st.session_state.pagina_atual == "info":
 
         st.markdown("### Como Utilizar")
         st.markdown("""
-        1. Navegue até a página de Configurações para ajustar os parâmetros do algoritmo conforme suas necessidades.
+        1. Navegue até a página de Configurações para enviar os arquivos CSV dos tutores e das escolas, e ajustar os parâmetros do algoritmo.
         2. Retorne à página inicial e clique no botão 'Otimizar' para executar o algoritmo de alocação.
         3. Após a conclusão, os resultados da otimização serão exibidos em uma tabela.
         """)
